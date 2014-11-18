0881ceeb67992351daee9c44f6c7fcd9bec50982
mrabins <markrabins@gmail.com>
2014-11-17 23:46:24 -0800

    Added users, and microposts via scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  resources :microposts
 
-  # You can have the root of your site routed with "root"
+  resources :users
   root 'application#hello'
 
   # Example of regular route:
